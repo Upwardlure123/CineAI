50ac3a5747a350a6a55760e8eed02fcb55a4e19c
vivek0106-source <vivek.gupta@sorted.team>
2025-01-04 17:28:46 +0530

    Configured vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-green-100 text-2xl">
+        Namaste everyone. Lets build Netflix-GPT
+      </div>
+    </>
+  );
+}
+
+export default App;
